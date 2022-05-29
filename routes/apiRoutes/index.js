41c108074a7f85c18c2a9988cd294198129deba2
hvphan <hvphan1993@gmail.com>
2022-05-28 20:11:32 -0700

    updated apiRoutes, added uuidv4

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+    createNote,
+    updateDb,
+} = require('../../lib/notes');
+
+const { notes } = require('../../db/notes');
+const { v4: uuidv4 } = require('uuid');
+
+// show notes in json data
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = uuidv4();
+    const newNote = createNote(req.body, notes);
+    res.json(newNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const params = req.params.id
+    updateDb(params, notes);
+    res.redirect('');
+});
+
+module.exports = router;
