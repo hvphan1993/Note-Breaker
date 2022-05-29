e7e6b3858ed65c3358bb1d63e87375ca178d81c3
hvphan <hvphan1993@gmail.com>
2022-05-29 15:11:35 -0700

    corrected pathing to enable save, changed format for db, tidied up code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,25 +1,24 @@
-const router = require('express').Router();
-const {createNote, updateDb} = require('../../lib/notes');
-
-const { notes } = require('../../db/db.json');
-const { v4: uuidv4 } = require('uuid');
+const router = require("express").Router();
+const { createNote, updateDb } = require("../../lib/notes");
+const { notes } = require("../../db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 // show notes in json data
-router.get('/notes', (req, res) => {
-    let results = notes;
-    res.json(results);
+router.get("/notes", (req, res) => {
+  let results = notes;
+  res.json(results);
 });
 
-router.post('/notes', (req, res) => {
-    req.body.id = uuidv4();
-    const newNote = createNote(req.body, notes);
-    res.json(newNote);
+router.post("/notes", (req, res) => {
+  req.body.id = uuidv4();
+  const newNote = createNote(req.body, notes);
+  res.json(newNote);
 });
 
-router.delete('/notes/:id', (req, res) => {
-    const params = req.params.id
-    updateDb(params, notes);
-    res.redirect('');
+router.delete("/notes/:id", (req, res) => {
+  const params = req.params.id;
+  updateDb(params, notes);
+  res.redirect("");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // parse incoming string/array data
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // parse incoming json data
 app.use(express.json());
 
 // Use apiRoutes
-app.use('./api', apiRoutes);
-app.use('/', htmlRoutes); 
-
-
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
